1610e69a837915e03b8f258e5bd6c17337db2031
Nick Murphy <nvmanis@bsu.edu>
2019-10-25 16:00:53 -0400

    styling, got rid of warning

--- GestaltTherapyApp/MyAssignments.js
+++ GestaltTherapyApp/MyAssignments.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import * as Font from 'expo-font';
 import {StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
@@ -8,29 +7,31 @@ export default class MyAssignments extends Component {
 
     static navigationOptions = {
         title: 'My Assignments',
+        header: (
+          <View style={{backgroundColor:'#7FE5F0'}}>
+            <View style={{width:300, height:70, backgroundColor:'#FFFFFF', alignSelf:'center', borderRadius:10, borderTopLeftRadius:0, borderTopRightRadius:0}}>
+              <Text style={{fontFamily:'sans-serif-condensed', fontSize:24, textAlign:'center', marginTop:25}}>My Assignments</Text>
+            </View>
+          </View>
+        ),
         headerStyle: {
-          
+          backgroundColor: 'transparent',
         },
-        headerTitleStyle: {
-            textAlign: 'center',
-            flex: 1,
-            fontSize:24,
-            fontWeight: '200',
-            //fontFamily: "Alegreya-Regular",
-        }
       };
 
     render() {
       return (
         <View style={styles.screen}>
           <View style={styles.container}>
-            <Text style={{textAlign:'center', fontSize:20, padding:5}}>Recent</Text>
-            <TouchableOpacity><Text style={{backgroundColor:'#F83839', color:'#FFFFFF', textAlign:'left', marginLeft:20, marginRight:20, marginTop:10, borderRadius:10, paddingLeft:10, paddingBottom:5, paddingTop:5}}>Assignment Four</Text></TouchableOpacity>
-            <Text style={{textAlign:'center', fontSize:20, padding:5, marginTop:30}}>Past</Text>
-            <Text style={{textAlign:'center', fontSize:12, padding:10, fontStyle:'italic'}}>These assignments have already been submitted but you can download your answers.</Text>
-            <TouchableOpacity><Text style={{backgroundColor:'#f5f5f5', color:'#a9a9a9', textAlign:'left', marginLeft:20, marginRight:20, marginTop:20, borderRadius:10, paddingLeft:10, paddingBottom:5, paddingTop:5}}>Assignment Three</Text></TouchableOpacity>
-            <TouchableOpacity><Text style={{backgroundColor:'#f5f5f5', color:'#a9a9a9', textAlign:'left', marginLeft:20, marginRight:20, marginTop:20, borderRadius:10, paddingLeft:10, paddingBottom:5, paddingTop:5}}>Assignment Two</Text></TouchableOpacity>
-            <TouchableOpacity><Text style={{backgroundColor:'#f5f5f5', color:'#a9a9a9', textAlign:'left', marginLeft:20, marginRight:20, marginTop:20, borderRadius:10, paddingLeft:10, paddingBottom:5, paddingTop:5}}>Assignment One</Text></TouchableOpacity>
+            <Text style={{textAlign:'center', fontFamily:'sans-serif-condensed', fontSize:20, padding:5}}>Recent</Text>
+            <TouchableOpacity>
+              <Text style={{ fontFamily:'serif', fontSize: 16, backgroundColor:'#F83839', color:'#FFFFFF', textAlign:'left', marginLeft:20, marginRight:20, marginTop:10, borderRadius:10, paddingLeft:10, paddingBottom:5, paddingTop:5}}>Assignment Four</Text>
+            </TouchableOpacity>
+            <Text style={{textAlign:'center', fontFamily:'sans-serif-condensed', fontSize:20, padding:5, marginTop:30}}>Past</Text>
+            <Text style={{textAlign:'center', fontSize:12, padding:5, fontFamily:'serif', fontStyle:'italic'}}>These assignments have already been submitted, but you can download your answers.</Text>
+            <TouchableOpacity><Text style={{fontFamily:'serif', fontSize: 16, backgroundColor:'#f5f5f5', color:'#a9a9a9', textAlign:'left', marginLeft:20, marginRight:20, marginTop:20, borderRadius:10, paddingLeft:10, paddingBottom:5, paddingTop:5}}>Assignment Three</Text></TouchableOpacity>
+            <TouchableOpacity><Text style={{fontFamily:'serif', fontSize: 16, backgroundColor:'#f5f5f5', color:'#a9a9a9', textAlign:'left', marginLeft:20, marginRight:20, marginTop:20, borderRadius:10, paddingLeft:10, paddingBottom:5, paddingTop:5}}>Assignment Two</Text></TouchableOpacity>
+            <TouchableOpacity><Text style={{fontFamily:'serif', fontSize: 16, backgroundColor:'#f5f5f5', color:'#a9a9a9', textAlign:'left', marginLeft:20, marginRight:20, marginTop:20, borderRadius:10, paddingLeft:10, paddingBottom:5, paddingTop:5}}>Assignment One</Text></TouchableOpacity>
           </View>
         </View>
       );
