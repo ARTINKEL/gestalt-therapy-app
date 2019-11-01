8364b4862f273bc0858fda74b35ac4a50e4c1f0f
Nick Murphy <nvmanis@bsu.edu>
2019-11-01 16:01:33 -0400

    Styled radio buttons

--- GestaltTherapyApp/MultipleChoice.js
+++ GestaltTherapyApp/MultipleChoice.js
@@ -6,25 +6,36 @@ export default class MultipleChoice extends Component {
     
     render(){
         var radio_props = [
-            {label: '1', value: 0 },
-            {label: '2', value: 1 }
+            {label: '1', value: 0},
+            {label: '2', value: 0},
+            {label: '3', value: 0},
+            {label: '4', value: 0},
+            {label: '5', value: 0}
           ];
+        state = {
+            value:0
+        }
         return (
-            <View>
+            <View style={{flex:1, alignItems:'center'}}>
+                <Text style={{textAlign:'center', fontFamily:'sans-serif-condensed', fontSize:16, padding:5}}>2. On a scale of 1 to 5, how did you feel about today's session?</Text>
                 <RadioForm
                     radio_props={radio_props}
                     initial={0}
+                    formHorizontal={true}
+                    labelHorizontal={false}
                     onPress={(value) => {this.setState({value:value})}}
                 >
-                    <RadioButtonInput
-                        onPress={console.log("hi")}
-                        borderWidth={1}
-                        buttonInnerColor={'#e74c3c'}
-                        buttonSize={40}
-                        buttonOuterSize={80}
-                        buttonStyle={{}}
-                        buttonWrapStyle={{marginLeft: 10}}
-                    />
+                    <RadioButton labelHorizontal={false}>
+                        <RadioButtonInput
+                            onPress={console.log("hi")}
+                            borderWidth={1}
+                            buttonInnerColor={'#e74c3c'}
+                            buttonSize={40}
+                            buttonOuterSize={80}
+                            buttonStyle={{}}
+                            buttonWrapStyle={{marginLeft: 10}}
+                        />
+                    </RadioButton>
                 </RadioForm>
             </View>
         )
