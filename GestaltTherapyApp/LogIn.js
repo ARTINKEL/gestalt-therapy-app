04bfe7402613bb64815b209eaaa9b3d3fba965dc
Nick Murphy <nvmanis@bsu.edu>
2020-01-14 01:36:08 -0500

    added state for the login form

--- GestaltTherapyApp/LogIn.js
+++ GestaltTherapyApp/LogIn.js
@@ -1,37 +1,93 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View, TextInput } from 'react-native';
+import {StyleSheet, Text, View, TextInput, Keyboard, Alert, KeyboardAvoidingView, ScrollView } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default class LogIn extends Component {
 
-    static navigationOptions = {
-        title: 'My Assignments',
-        header: (
-          <View style={{backgroundColor:'#7FE5F0'}}>
-          </View>
-        ),
-        headerStyle: {
-          backgroundColor: 'transparent',
-        },
-      };
+    constructor(props) {
+      super(props);
+      this.state = { 
+        userID: '',
+        password:'' 
+      }
+      this.handleIDChange = this.handleIDChange.bind(this);
+      this.handlePasswordChange = this.handlePasswordChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
       
+    handleIDChange(userID) {
+      this.setState({ userID });
+    }
+
+    handlePasswordChange(password) {
+      this.setState({ password });
+    }
+
+    handleSubmit() {
+      Alert.alert('Login information received', 'User ID: ' + this.state.userID + '\n' + 'Password: ' + this.state.password)
+      this.props.navigation.navigate('MyAssignments')
+    }
+
+    static navigationOptions = {
+      title: 'My Assignments',
+      header: (
+        <View style={{backgroundColor:'#7FE5F0'}}/>
+        ),
+      headerStyle: {
+        backgroundColor: 'transparent',
+      },
+    };
+
     render() {
         return (
             <View style={styles.screen}>
+              <KeyboardAvoidingView style={{ flex: 1, flexDirection: 'column',justifyContent: 'center',}} behavior="padding" enabled   keyboardVerticalOffset={0}>
+	            <ScrollView>
                 <View style={styles.head}>
                     <Text style={{backgroundColor:'#C8F4F9', width:200, fontSize:32}}>Working Title</Text>
                 </View>
                 <View style={styles.container}>
+                  
                     <Text style={{textAlign:'center', fontSize:20, marginBottom:20}}>Welcome Back</Text>
+
                     <Text style={{fontSize:16, marginLeft:15}}>ID Number</Text>
-                    <TextInput style={{alignSelf:'center', textAlign:'left', textAlignVertical:'top', margin:10, padding:10, paddingTop:8, width:250, height:40, borderColor:'#000000', borderWidth:1, borderRadius:10,}} editable maxLength={4} placeholder={'Your 4-digit code'}></TextInput>
+                    <TextInput 
+                      style={{alignSelf:'center', textAlign:'left', textAlignVertical:'top', margin:10, padding:10, paddingTop:8, width:250, height:40, borderColor:'#000000', borderWidth:1, borderRadius:10,}} 
+                      editable 
+                      maxLength={4} 
+                      placeholder={'Your 4-digit code'}
+                      value={this.state.userID}
+                      onChangeText={this.handleIDChange}
+                    />
+
                     <Text style={{fontSize:16, marginLeft:15}}>Password</Text>
-                    <TextInput style={{alignSelf:'center', textAlign:'left', textAlignVertical:'top', margin:10, padding:10, paddingTop:8, width:250, height:40, borderColor:'#000000', borderWidth:1, borderRadius:10,}} editable maxLength={4} placeholder={'Your 4-digit code'}></TextInput>
+                    <TextInput 
+                      style={{alignSelf:'center', textAlign:'left', textAlignVertical:'top', margin:10, padding:10, paddingTop:8, width:250, height:40, borderColor:'#000000', borderWidth:1, borderRadius:10,}} 
+                      editable 
+                      secureTextEntry={true}
+                      maxLength={4} 
+                      placeholder={'Your 4-digit code'}
+                      value={this.state.password}
+                      onChangeText={this.handlePasswordChange}
+                    />
+
                     <View style={{alignSelf:'center', width:150}}>
-                        <TouchableOpacity onPress={() => this.props.navigation.navigate('MyAssignments')}><Text style={{ fontFamily:'serif', fontSize: 16, backgroundColor:'#F83839', color:'#FFFFFF', alignSelf:'center', textAlign:'center', marginLeft:20, marginRight:20, marginTop:10, borderRadius:10, paddingTop:5, paddingBottom:5, paddingLeft:10, paddingRight:10}}>Log In</Text></TouchableOpacity>
-                        <TouchableOpacity onPress={() => this.props.navigation.navigate('AssignmentBuilder')}><Text style={{ fontFamily:'serif', fontSize: 16, backgroundColor:'#F83839', color:'#FFFFFF', alignSelf:'center', textAlign:'center', marginLeft:20, marginRight:20, marginTop:10, borderRadius:10, paddingTop:5, paddingBottom:5, paddingLeft:10, paddingRight:10}}>Build</Text></TouchableOpacity>
+                        <TouchableOpacity 
+                          onPress={this.handleSubmit}
+                        >
+                        <Text style={{ fontFamily:'serif', fontSize: 16, backgroundColor:'#F83839', color:'#FFFFFF', alignSelf:'center', textAlign:'center', marginLeft:20, marginRight:20, marginTop:10, borderRadius:10, paddingTop:5, paddingBottom:5, paddingLeft:10, paddingRight:10}}>Log In</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity 
+                          onPress={() => this.props.navigation.navigate('AssignmentBuilder')}
+                          >
+                          <Text style={{ fontFamily:'serif', fontSize: 16, backgroundColor:'#F83839', color:'#FFFFFF', alignSelf:'center', textAlign:'center', marginLeft:20, marginRight:20, marginTop:10, borderRadius:10, paddingTop:5, paddingBottom:5, paddingLeft:10, paddingRight:10}}>Build</Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
+              </ScrollView>
+              </KeyboardAvoidingView>
             </View>
         );
     }
